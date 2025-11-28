2c5c82fc8fe3636934f204f197c98749d0aad101
farrukh <107911430+fkhyu@users.noreply.github.com>
2025-11-28 22:42:10 +0200

    Add ArtNet module for DMX control and initialize camera module

---
+++ artnet-module.py
@@ -0,0 +1,23 @@
+import asyncio
+from pyartnet import ArtNetNode
+
+async def main():
+
+    async with ArtNetNode.create('10.1.1.104', 6454) as node:
+
+        # Create universe 6
+        universe = node.add_universe(6)
+
+        # Add a channel to the universe which consists of 3 values
+        # Default size of a value is 8Bit (0..255) so this would fill
+        # the DMX values 1..3 of the universe
+        channel = universe.add_channel(start=1, width=3)
+
+        # Fade channel to 255,0,0 in 5s
+        # The fade will automatically run in the background
+        channel.add_fade([255,0,0], 5000)
+
+        # this can be used to wait till the fade is complete
+        await channel
+
+asyncio.run(main())
